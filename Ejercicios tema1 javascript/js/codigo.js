5f9868c69e81dcf90ce696c46ad02f90661c7254
elviajerobarato <77701994+elviajerobarato@users.noreply.github.com>
2026-01-06 00:04:48 +0100

    Add files via upload

---
+++ Ejercicios tema1 javascript/js/codigo.js
@@ -0,0 +1,3 @@
+alert("Hola Mundo!");
+alert("Soy el primer script");
+/*Todo el código JavaScript se encuentre en un archivo externo llamado codigo.js, se muestran dos mensajes, primero Hola Mundo, y luego Soy el primer script */
